e0378a5be63feb5744c487b6dc74cf86a22f1e25
loiccotte <159036568+loiccotte@users.noreply.github.com>
2025-05-19 16:34:17 +0200

    Geocodage

--- App/app.R
+++ App/app.R
@@ -1,78 +1,51 @@
-# app.R
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
 library(shiny)
-library(bslib)
-library(leaflet)
-library(sf)
-library(dplyr)
 
-# --- Chargement / préparation des données
-# Exemple de dataframe fourni par l'utilisateur
-df=read.csv("Communes10-20K-Maslow.csv",sep=";")
-
-# Charger le shapefile ou GeoJSON des communes (INSEE_COM à adapter)
-communes_sf <- st_read("communes.geojson", quiet = TRUE)
-# Jointure avec les données Maslow
-communes_data <- communes_sf %>%
-  left_join(df_maslow, by = c("INSEE_COM" = "COM"))
-
-# --- UI
+# Define UI for application that draws a histogram
 ui <- fluidPage(
-  theme = bs_theme(bootswatch = "cerulean"),
-  tags$head(
-    # CSS personnalisé (optionnel)
-    tags$link(rel = "stylesheet", href = "style.css")
-  ),
-  titlePanel("Carte des besoins selon Maslow par commune"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      width = 3,
-      selectInput(
-        "niveau", "Sélectionnez un étage de la pyramide :",
-        choices = c(
-          "Physiologique", "Sécurité", "Appartenance",
-          "Estime", "Actualisation.de.soi", "Cognitif"
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
         ),
-        selected = "Physiologique"
-      )
-    ),
-    
-    mainPanel(
-      width = 9,
-      leafletOutput("map", height = "75vh")
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
     )
-  )
 )
 
-# --- Serveur
-server <- function(input, output, session) {
-  
-  output$map <- renderLeaflet({
-    req(input$niveau)
-    
-    # Palette adaptée aux valeurs sélectionnées
-    vals <- communes_data[[input$niveau]]
-    pal <- colorBin("YlOrRd", domain = vals, bins = 5, na.color = "transparent")
-    
-    leaflet(communes_data) %>%
-      addTiles() %>%
-      addPolygons(
-        fillColor   = ~pal(vals),
-        weight      = 0.5,
-        color       = "#444444",
-        fillOpacity = 0.8,
-        highlightOptions = highlightOptions(
-          weight = 2, color = "#666", bringToFront = TRUE
-        ),
-        label = ~paste0(Commune, ": ", vals),
-        labelOptions = labelOptions(direction = "auto")
-      ) %>%
-      addLegend(
-        pal = pal, values = vals, opacity = 0.8,
-        title = input$niveau, position = "bottomright"
-      )
-  })
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
 }
 
-# --- Lancement de l'application
-shinyApp(ui, server)
+# Run the application 
+shinyApp(ui = ui, server = server)
